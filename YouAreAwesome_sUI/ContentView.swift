d15aeb57eb26f107da0682fa48cecf69537b4c46
AntonyBil <beletskiy.ant@gmail.com>
2023-06-20 19:59:33 +0300

    Lern SwiftUI

---
+++ YouAreAwesome_sUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YouAreAwesome_sUI
+//
+//  Created by apple on 20.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouAreAwesome_sUI/YouAreAwesome_sUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome_sUIApp.swift
+//  YouAreAwesome_sUI
+//
+//  Created by apple on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome_sUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
