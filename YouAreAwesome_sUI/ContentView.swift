ffbed13bc63b6ad81843489d81cd07edf818931f
AntonyBil <beletskiy.ant@gmail.com>
2023-06-25 19:33:58 +0300

    moModified the button

--- YouAreAwesome_sUI/ContentView.swift
+++ YouAreAwesome_sUI/ContentView.swift
@@ -9,68 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     
- @State private var messageString = "Namaste"
+    @State private var messageString = ""
     
     var body: some View {
         
-            VStack {
-                
-                Spacer()
-                Image(systemName: "cloud.sun.rain.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .symbolRenderingMode(.multicolor)
-                    .padding()
-                    .background(Color("SkyBlue"))
-                    .cornerRadius(30)
-                    .shadow(color: .gray, radius: 20)
-                    .overlay(
-                         RoundedRectangle(cornerRadius: 30)
-                            .stroke(.teal, lineWidth: 1)
-                    )
-                    .padding()
-                   
-                   
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                   
-                
-                Spacer()
-                
-//                Divider()
-//                    .background(.black)
-//                    .padding()
-//                    .frame(width: 150.0)
-                
-//                Rectangle()
-//                    .fill(.indigo)
-//                    .frame(width: geometry.size.width * (2/3), height: 1)
-                
-                HStack {
-                    Button("Awesome") {
-                        messageString = "You are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Grete") {
-                        messageString = "You are Grate!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                //            .border(.green, width: 3)
+        VStack {
+            
+            Spacer()
+            //                Image(systemName: "cloud.sun.rain.fill")
+            //                    .resizable()
+            //                    .scaledToFit()
+            //                    .symbolRenderingMode(.multicolor)
+            //                    .padding()
+            //                    .background(Color("SkyBlue"))
+            //                    .cornerRadius(30)
+            //                    .shadow(color: .gray, radius: 20)
+            //                    .overlay(
+            //                         RoundedRectangle(cornerRadius: 30)
+            //                            .stroke(.teal, lineWidth: 1)
+            //                    )
+            //                    .padding()
+            //
+            
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
                 .padding()
+            
+            
+            Spacer()
+            
+            //                Divider()
+            //                    .background(.black)
+            //                    .padding()
+            //                    .frame(width: 150.0)
+            
+            //                Rectangle()
+            //                    .fill(.indigo)
+            //                    .frame(width: geometry.size.width * (2/3), height: 1)
+            
+            
+            Button("Show massage") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Grete!"
+                
+                if messageString == message1 {
+                    messageString = message2
+                } else {
+                    messageString = message1
+                }
             }
-       
+            .buttonStyle(.borderedProminent)
+           
+        }
+        
     }
 }
 
